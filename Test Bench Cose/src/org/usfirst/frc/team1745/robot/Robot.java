fe250fc6fc627f2a288b2d91ee561a97cb70635e
adam <Deveveloper@WINDOWS-5MP2JIN.richardson.k12.tx.us>
2015-01-13 17:42:59 -0600

    Merging tested robotdemo code to branch - Added 10% speed to victor

--- Test Bench Cose/src/org/usfirst/frc/team1745/robot/Robot.java
+++ Test Bench Cose/src/org/usfirst/frc/team1745/robot/Robot.java
@@ -5,13 +5,10 @@ package org.usfirst.frc.team1745.robot;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.Joystick.ButtonType;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.Trigger.ButtonScheduler;
 import edu.wpi.first.wpilibj.hal.CanTalonSRX;
 
 /**
@@ -30,71 +27,67 @@ import edu.wpi.first.wpilibj.hal.CanTalonSRX;
  * don't. Unless you know what you are doing, complex code will be much more difficult under
  * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
-public class Robot extends SampleRobot 
-{
-    Compressor myCompressor;
+public class Robot extends SampleRobot {
+    RobotDrive myRobot;
     CanTalonSRX myTalon;
     DoubleSolenoid mySolenoid;
     Joystick stick;
+    Compressor myCompressor;
 
-    public Robot() 
-    {
-       stick = new Joystick(0);
-       myCompressor = new Compressor(2);
-       myTalon = new CanTalonSRX(3);
-       mySolenoid = new DoubleSolenoid(2,0,1);
+    public Robot() {
+        myRobot = new RobotDrive(0, 1);
+        myRobot.setExpiration(0.1);
+        myTalon = new CanTalonSRX(3);
+        mySolenoid = new DoubleSolenoid( 2, 0, 1);
+        myCompressor = new Compressor(2);
+        stick = new Joystick(0);
     }
 
-   
-    public void autonomous() 
-    {
-    	while (isAutonomous() && isEnabled())
-    	 // if the pressure is below switch preset turn on the compressor. else turn it off.
-        if(myCompressor.getPressureSwitchValue())
-        {
-        	myCompressor.start();
-        }
-        else
-        {
-        	myCompressor.start();
-        }
+    /**
+     * Drive left & right motors for 2 seconds then stop
+     */
+    public void autonomous() {
+       // myRobot.setSafetyEnabled(false);
+       // myRobot.drive(-0.5, 0.0);	// drive forwards half speed
+       // Timer.delay(2.0);		//    for 2 seconds
+       // myRobot.drive(0.0, 0.0);	// stop robot
     }
 
-    
+    /**
+     * Runs the motors with arcade steering.
+     */
     public void operatorControl() {
-        
-        while (isOperatorControl() && isEnabled()) 
-        {
-          	// Map the Y axis of the joystick to the talon
-        	myTalon.Set(stick.getY());
-            
-            // if the pressure is below switch preset turn on the compressor. else turn it off.
+        myRobot.setSafetyEnabled(true);
+        while (isOperatorControl() && isEnabled()) {
+           // myRobot.arcadeDrive(stick); // drive with arcade style (use right stick)
+        	myTalon.Set(stick.getY()*0.1);
             if(myCompressor.getPressureSwitchValue())
             {
-            	myCompressor.start();
+            	myCompressor.stop();
             }
             else
             {
             	myCompressor.start();
-            }
-            // if the trigger is presses actuate the solenoid
-            if(stick.getButton(ButtonType.kTrigger))
-            {
-            	mySolenoid.set(Value.kForward);
-            }
-            else
-            {
-            	mySolenoid.set(Value.kReverse);
+            	
             }
             	
-            Timer.delay(0.005);		// wait for a motor update time
+        	if(stick.getRawButton(1))
+            	mySolenoid.set(Value.kForward);
+            else
+            	mySolenoid.set(Value.kReverse);
+        	//System.out.println("Tallon Value: " + myTalon..toString());
+        	System.out.println("Solenoid Switch Value: " + mySolenoid.get().toString());
+        	System.out.println("Compessor Switch Value: " + myCompressor.getPressureSwitchValue());
+        	Timer.delay(0.005);		// wait for a motor update time
         }
+        
     }
+    
+    
 
     /**
      * Runs during test mode
      */
-    public void test() 
-    {
+    public void test() {
     }
 }
