82716593ca88a3ac5b0fd6573c9f2229767504f6
AdamGoat <goad.adam@gmail.com>
2015-01-14 00:48:01 -0600

    Lots of changes, possible drive code -added Talons for all motors -changed "CanTalonSRX" to "CANTalon"-required -added polar-mecanum drive code on x-axis, y-axis, and twist -moved lift to throtle control from y-axis -added gamepad-unused -added println to test mode

--- Test Bench Cose/src/org/usfirst/frc/team1745/robot/Robot.java
+++ Test Bench Cose/src/org/usfirst/frc/team1745/robot/Robot.java
@@ -2,12 +2,14 @@
 package org.usfirst.frc.team1745.robot;
 
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.hal.CanTalonSRX;
 
@@ -27,26 +29,50 @@ import edu.wpi.first.wpilibj.hal.CanTalonSRX;
  * don't. Unless you know what you are doing, complex code will be much more difficult under
  * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
-public class Robot extends SampleRobot {
+public class Robot extends SampleRobot 
+{
     RobotDrive myRobot;
-    CanTalonSRX myTalon;
+    CANTalon myTalonZero; // Lift
+    CANTalon myTalonOne; //Front Left
+    CANTalon myTalonTwo; //Back Left
+    CANTalon myTalonThree; //Front Right
+    CANTalon myTalonFour; //Back Right
     DoubleSolenoid mySolenoid;
     Joystick stick;
+    Joystick gamepad;
     Compressor myCompressor;
+    /*SpeedController frontLeftMotor;
+    SpeedController frontRightMotor;
+    SpeedController backLeftMotor;
+    SpeedController backRightMotor;*/
+    
+    
 
-    public Robot() {
-        myRobot = new RobotDrive(0, 1);
-        myRobot.setExpiration(0.1);
-        myTalon = new CanTalonSRX(3);
+    public Robot() 
+    {
+       
+        myTalonZero = new CANTalon(0); // Lift
+        myTalonOne = new CANTalon(1); // Front Left
+        myTalonTwo = new CANTalon(2); // Back left
+        myTalonThree = new CANTalon(3); // Front Right
+        myTalonFour = new CANTalon(4); // Back Right
+       
         mySolenoid = new DoubleSolenoid( 2, 0, 1);
         myCompressor = new Compressor(2);
+        
         stick = new Joystick(0);
+        gamepad = new Joystick(1);
+       
+        myRobot = new RobotDrive(myTalonOne,myTalonTwo,myTalonThree,myTalonFour);
+        myRobot.setExpiration(0.1);
+        
     }
 
     /**
      * Drive left & right motors for 2 seconds then stop
      */
-    public void autonomous() {
+    public void autonomous() 
+    {
        // myRobot.setSafetyEnabled(false);
        // myRobot.drive(-0.5, 0.0);	// drive forwards half speed
        // Timer.delay(2.0);		//    for 2 seconds
@@ -54,13 +80,20 @@ public class Robot extends SampleRobot {
     }
 
     /**
-     * Runs the motors with arcade steering.
+     * Runs the motors with Mecanum Drive and lift.
      */
-    public void operatorControl() {
+    public void operatorControl() 
+    {
         myRobot.setSafetyEnabled(true);
-        while (isOperatorControl() && isEnabled()) {
-           // myRobot.arcadeDrive(stick); // drive with arcade style (use right stick)
-        	myTalon.Set(stick.getY()*0.1);
+        while (isOperatorControl() && isEnabled()) 
+        {
+           // spin motor for lift on joy stick throttle
+        	myTalonZero.set(stick.getThrottle()*0.1);
+        	
+        	//Mecanum drive
+        	myRobot.mecanumDrive_Polar(stick.getX(), stick.getY(), stick.getTwist());
+        	
+        	//Turn on compressor if more air is needed
             if(myCompressor.getPressureSwitchValue())
             {
             	myCompressor.stop();
@@ -68,16 +101,18 @@ public class Robot extends SampleRobot {
             else
             {
             	myCompressor.start();
-            	
             }
-            	
+            
+            //Actuate Solenoid for claw
         	if(stick.getRawButton(1))
             	mySolenoid.set(Value.kForward);
             else
             	mySolenoid.set(Value.kReverse);
+        	
         	//System.out.println("Tallon Value: " + myTalon..toString());
         	System.out.println("Solenoid Switch Value: " + mySolenoid.get().toString());
         	System.out.println("Compessor Switch Value: " + myCompressor.getPressureSwitchValue());
+        	
         	Timer.delay(0.005);		// wait for a motor update time
         }
         
@@ -88,6 +123,8 @@ public class Robot extends SampleRobot {
     /**
      * Runs during test mode
      */
-    public void test() {
+    public void test() 
+    {
+    	System.out.println("Welcome to Test Mode!");
     }
 }
